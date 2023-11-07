c640bf00fa5ef8a0db556a5fa06ea140cb639ddd
abdulhay103 <abdulhay.me@gmail.com>
2023-11-07 12:14:48 +0600

    Initial commit from Create Next App setup processing

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+    return (
+        <main>
+            <h1>Jara Init1</h1>
+            <h3>ok</h3>
+        </main>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        screens: {
+            large: { max: "1600px" },
+            middle: { max: "1040px" },
+            small: { max: "768px" },
+        },
+        fontFamily: {
+            roboto: ["Roboto", "sans-serif"],
+            syne: ["Syne", "sans-serif"],
+        },
+        extend: {
+            colors: {
+                "main-color": "var(--main-color)",
+                "heading-color": "var(--heading-color)",
+                "main-gradient": "var(--main-gradient)",
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
